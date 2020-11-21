407897082d479cfb9be9a82c44196966c1238cbe
Sam Weru <samweru@gmail.com>
2020-11-21 04:36:56 +0300

    store raised exeception in iteratable message list

--- src/Strukt/Raise.php
+++ src/Strukt/Raise.php
@@ -4,8 +4,32 @@ namespace Strukt;
 
 class Raise{
 
+	private static $messages = [];
+	private static $limit = 9;
+
 	public function __construct($error, $code = null){
 
+		if(count(static::$messages) > static::$limit)
+			array_shift(static::$messages);
+
+		static::$messages[] = $error;
+
 		throw new \Exception($error, $code);
+	}
+
+	public static function setStoreLimit($limit){
+
+		static::$limit = $limit;
+	}
+
+	public static function getMessages(){
+
+		return new class(static::$messages) extends Type\Arr{
+
+			public function __construct(&$messages){
+
+				parent::__construct($messages);
+			}
+		};
 	}
 }
