cb9adc596a0a2bc84f1452038a705e93273d56c3
pitsolu <pitsolu@gmail.com>
2023-09-15 12:19:40 +0300

    upgraded to php8.1 with more helpers

--- src/helpers.php
+++ src/helpers.php
@@ -3,20 +3,46 @@
 use Strukt\Builder\Collection as CollectionBuilder;
 use Strukt\Core\Collection;
 use Strukt\Core\Today;
+use Strukt\Contract\AbstractArr;
+use Strukt\Type\Arr;
+use Strukt\Type\Json;
+use Strukt\Env;
 
-if(!function_exists("bunch")){
+if(!function_exists("collect")){
 
-	function bunch(array $assoc){
+	function collect(array $assoc){
 
 		return CollectionBuilder::create(new Collection())->fromAssoc($assoc);
 	}
 }
 
-if(!function_exists("band")){
+if(!function_exists("arr")){
 
-	function band(array $bundle){
+	function arr(array $bundle){
 
-		return new \Strukt\Type\Arr($bundle);
+		return new class($bundle) extends \Strukt\Contract\AbstractArrOps{
+
+			private $val;
+
+			public function __construct(array $bundle){
+
+				$this->val = $bundle;
+			}
+
+			/**
+			* Is array fully associative
+			*/
+			public function isMap(){
+
+				return AbstractArr::isMap($this->val);
+			}
+
+
+			public function level(){
+
+				return Arr::level($this->val);
+			}
+		};
 	}
 }
 
@@ -72,5 +98,67 @@ if(!function_exists("today")){
 	function today(){
 
 		return new Today();
+	}
+}
+
+if(!function_exists("env")){
+
+	function env(string $key, string|array|int $val = null){
+
+		if(!empty($val) && !is_null($val))
+			Env::set($key, $val);
+
+		return Env::get($key);
+	}
+}
+
+if(!function_exists("reg")){
+
+	function reg(string $key = null){
+
+		$reg = Strukt\Core\Registry::getSingleton();
+		if(!is_null($key))
+			return $reg->get($key);
+
+		return $reg;
+	}
+}
+
+if(!function_exists("json")){
+
+	function json(string|array $obj){
+
+		return new class($obj){
+
+			private $obj;
+
+			public function __construct($obj){
+
+				if(is_array($obj))
+					$obj = Json::encode($obj);
+
+				$this->obj = $obj;
+			}
+
+			public function pp(){
+
+				return Json::pp($this->obj);
+			}
+
+			public function decode(){
+
+				return Json::decode($this->obj);
+			}
+
+			public function encode(){
+
+				return $this->obj;
+			}
+
+			public function valid(){
+
+				return Json::isJson($this->obj);
+			}
+		};
 	}
 }
